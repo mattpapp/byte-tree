e4b038dfaae29980d16d1b7ac530d7b7b7afb48d
Matthew Pappas <matteopappas@gmail.com>
2025-09-26 11:26:51 +0200

    Apply spotless

--- src/main/java/com/mattpapp/bytetree/SortedByteTree.java
+++ src/main/java/com/mattpapp/bytetree/SortedByteTree.java
@@ -1,6 +1,7 @@
 package com.mattpapp.bytetree;
 
 public interface SortedByteTree {
-    void put(byte[] key, byte[] value);
-    byte[] get(byte[] key);
+  void put(byte[] key, byte[] value);
+
+  byte[] get(byte[] key);
 }
